6c4af5e592eca1f2fff20799c30a7ba3733ce044
Tom <150679896+cqptomii@users.noreply.github.com>
2023-11-15 20:26:24 +0100

    Update RPremisse.c
    fix  name structures + prototype fonction ajouterqP

--- SystemExpert/RegleF/RPremisse.c
+++ SystemExpert/RegleF/RPremisse.c
@@ -1,31 +1,26 @@
 #include "RPremisse.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include "stdbool.h"
-#include "string.h"
-
 
 Regle *creerRegle() {
     Regle *regle = (Regle *)malloc(sizeof(Regle));
-    regle->premisse = NULL;
+    regle->prem = NULL;
     regle->conclusion = NULL;
     return regle;
 }
 
-void ajouterqP(ListeProp **premisse, Proposition prop)
+void ajouterqP(premisse *prem, Proposition prop)
 {
-    ListeProp *nvprop = (ListeProp *)malloc(sizeof(ListeProp));
+    premElement *nvprop = (premElement *)malloc(sizeof(premElement));
     if (nvprop == NULL) {
         exit(EXIT_FAILURE);
     }
     nvprop->proposition = prop;
     nvprop->next = NULL;
 
-    if (*premisse == NULL) {
-        *premisse = nvprop;
+    if (prem == NULL) {
+        prem = nvprop;
     } else {
         // parcours de la premisse
-        ListeProp *tmp = *premisse;
+        premElement *tmp = prem;
         while (tmp->next != NULL) {
             tmp = tmp->next;
         }
@@ -39,38 +34,39 @@ Regle *ajouterprop(Regle *regle, Proposition prop) {
     return regle;
 }
 
-bool appartient(ListeProp *premisse, Proposition prop) {
-    if (premisse == NULL) {
+bool appartient(premisse *prem, Proposition prop) {
+    if (prem == NULL) {
         return false;
     }
     else if (strcmp(premisse->proposition.phrase, prop.phrase) == 0) {
         return true;
     } else {
-        return appartient(premisse->next, prop);
+        return appartient(prem->next, prop);
     }
 }
 // SUPPRIME QUE EN QUEUE PR INSTANT
 Regle *suppprop(Regle *regle) {
-    if (regle->premisse == NULL) {
+    if (regle->prem == NULL) {
         return regle;
     }
-    else if (regle->premisse->next == NULL) {
+    else if (regle->prem->next == NULL) {
+        free(regle->prem);
         return NULL;
     }
     else {
         Regle *tmp = regle;
-        while (tmp->premisse->next != NULL) {
-            tmp = tmp->premisse->next;
+        while (tmp->prem->next != NULL) {
+            tmp = tmp->prem->next;
         }
-        tmp->premisse->before->next = NULL; // fais pointer avant dernier sur null
-        free(tmp->premisse); // supp dernier
+        tmp->prem->before->next = NULL; // fais pointer avant dernier sur null
+        free(tmp->prem); // supp dernier
 
     }
 }
 
 bool estVidePremisse(Regle *regle)
 {
-    if (regle->premisse == NULL) {
+    if (regle->prem == NULL) {
         return true;
     }
     else
